a56d9eb111c64dbed1e9590c0e19fed98ede4e3f
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2023-06-02 17:27:46 +0200

    set sqlite dbcontext

---
+++ NoteAPI.IntegrationTests/Helpers/NoteDbContextSQLLite.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoteAPI.IntegrationTests.Helpers;
+
+public class NoteDbContextSQLLite : Persistence.NoteDbContext
+{
+    public NoteDbContextSQLLite(DbContextOptions<Persistence.NoteDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+        Database.Migrate();
+    }
+}
